934e1c663cc5ececb5516e46ce52c639917fe81e
gitprajapati <77141376+gitprajapati@users.noreply.github.com>
2022-07-25 12:15:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import streamlit as st
 st.title('Multiplication of 2 numbers')
-st.subheader("Enter two numbers or use +/- button")
 a=st.number_input('Enter 1st number')
 b=st.number_input('Enter 2nd number')
 st.write("Multiplication of 2 numbers is = ",a*b)
